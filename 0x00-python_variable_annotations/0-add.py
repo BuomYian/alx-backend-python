6d0693703fc5dc132f0c91744e3e3b7c48fe48b3
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-07-07 20:16:55 +0300

    float returned

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,5 +4,6 @@ A type-annotated function takes a float a and a float b as arguments and returns
 """
 
 
-def add(a: float, b: float):
+def add(a: float, b: float) -> float:
+    """Add two float and return their sum"""
     return a + b
