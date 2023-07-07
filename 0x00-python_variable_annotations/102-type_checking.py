fac1c42f6bffd562db720c6d4413e82455ad00c5
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-07-07 22:00:34 +0300

    fixed

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
+
 """
-Use mypy to validate the following piece of code and apply any necessary changes.
+This module provides a function to zoom in the elements of a tuple by repeating them.
 """
 
 from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
-    """Zoom in the elements of a tuple by repeating them."""
+    """
+    Zoom in the elements of a tuple by repeating them.
+
+    Args:
+        lst (Tuple[int, ...]): The input tuple.
+        factor (int, optional): The zoom factor. Defaults to 2.
+
+    Returns:
+        Tuple[int, ...]: The zoomed-in tuple.
+    """
     zoomed_in: List[int] = [
         item
         for item in lst
         for _ in range(factor)
     ]
     return tuple(zoomed_in)
-
-
-array: Tuple[int, int, int] = (12, 72, 91)
-
-zoom_2x: Tuple[int, int, int, int, int, int] = zoom_array(array)
-
-zoom_3x: Tuple[int, int, int, int, int, int,
-               int, int, int] = zoom_array(array, 3)
