75acb9b61ceef94b5354b9f2e07b4d68daf0cd12
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-07-14 15:37:55 +0300

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+A coroutine that take no arguments
+"""
+
+import asyncio
+import random
+from typing import AsyncIterator
+
+
+async def async_generator() -> AsyncIterator[float]:
+    """
+    Coroutine that generates random numbers asynchronously.
+
+    Yields:
+        float: Random number between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
